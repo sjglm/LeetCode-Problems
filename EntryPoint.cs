67d6d3d3137071777280e014c6db337dde41b0f8
sjglm <mattyglockhart@aol.com>
2024-03-04 16:19:49 +0000

    Updated user display

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -31,47 +31,56 @@ public class EntryPoint
                 case 9:
                     Console.WriteLine("Running Problem - 9. Palindrome Number\n");
                     PalindromeNumberClass.InternalMain();
+                    Console.WriteLine("\nPress any key to exit...");
                     Console.ReadKey();
                     break;
                 case 14:
                     Console.WriteLine("Running Problem - 14. Longest Common Prefix\n");
                     Console.WriteLine("Under Construction...");
                     //LongestCommonPrefixClass.InternalMain();
+                    Console.WriteLine("\nPress any key to exit...");
                     Console.ReadKey();
                     break;
                 case 20:
                     Console.WriteLine("Running Problem - 20. Valid Parentheses\n");
                     ValidParenthesesClass.InternalMain();
+                    Console.WriteLine("\nPress any key to exit...");
                     Console.ReadKey();
                     break;
                 case 26:
                     Console.WriteLine("Running Problem - 26. Remove Duplicates from Sorted Array\n");
                     RemoveDuplicatesFromSortedArrayClass.InternalMain();
+                    Console.WriteLine("\nPress any key to exit...");
                     Console.ReadKey();
                     break;
                 case 27:
                     Console.WriteLine("Running Problem - 27. Remove Element\n");
                     RemoveElementClass.InternalMain();
+                    Console.WriteLine("\nPress any key to exit...");
                     Console.ReadKey();
                     break;
                 case 28:
                     Console.WriteLine("Running Problem - 28. Find the Index of the First Occurrence in a String\n");
                     IndexOfTheFirstOccurenceInAStringClass.InternalMain();
+                    Console.WriteLine("\nPress any key to exit...");
                     Console.ReadKey();
                     break;
                 case 58:
                     Console.WriteLine("Running Problem - 58. Length of Last Word\n");
                     LengthOfLastWordClass.InternalMain();
+                    Console.WriteLine("\nPress any key to exit...");
                     Console.ReadKey();
                     break;
                 case 66:
                     Console.WriteLine("Running Problem - 66. Plus One\n");
                     PlusOneClass.InternalMain();
+                    Console.WriteLine("\nPress any key to exit...");
                     Console.ReadKey();
                     break;
                 case 125:
                     Console.WriteLine("Running Problem - 125. Valid Palindrome\n");
                     ValidPalindromeClass.InternalMain();
+                    Console.WriteLine("\nPress any key to exit...");
                     Console.ReadKey();
                     break;
             }
